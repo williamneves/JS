7efdcd8957f6f0608e6b29b6f6dd90563d03deda
William Neves <drwilliamneves@gmail.com>
2022-02-28 01:36:33 -0500

    fining finishin

--- button_clicker/script.js
+++ button_clicker/script.js
@@ -12,18 +12,18 @@ function hiddenObj(element){
 var numberpost1 = 24
 function alertChangeLike1(element){
     numberpost1++
-    alert("this got 1 more like"+numberpost1+" likes"   )
+    alert("This got 1 more like, now is "+numberpost1+" likes")
     element.innerText = numberpost1 + " likes"    
 }
 var numberpost2 = 99
 function alertChangeLike2(element){
     numberpost2++
-    alert("this got 1 more like"+numberpost2+" likes"   )
+    alert("This got 1 more like, now is "+numberpost2+" likes")
     element.innerText = numberpost2 + " likes"    
 }
 var numberpost3 = 18
 function alertChangeLike3(element){
     numberpost3++
-    alert("this got 1 more like"+numberpost3+" likes"   )
+    alert("This got 1 more like, now is "+numberpost3+" likes")
     element.innerText = numberpost3 + " likes"    
 }
