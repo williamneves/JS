953ca3a10289534d6f9aa1f2ca046a1aba0c65c2
William Neves <drwilliamneves@gmail.com>
2022-02-28 15:46:34 -0500

    fine js adjustments with the buttons and this elements

--- button_clicker/script.js
+++ button_clicker/script.js
@@ -1,9 +1,17 @@
+function drawOnOff(){
+    var content = '<button onclick="onOrOff(this)"><i class="fa-solid fa-right-to-bracket"></i> LOGIN</button>';
+    document.getElementById("button-login").innerHTML = content;
+}
+drawOnOff()
+
 function onOrOff(element){
-    if (element.innerText == 'LOGIN'){
-        element.innerText = "LOGOFF"
+    console.log(element.innerText)
+    if (element.innerText == ' LOGIN'){
+        console.log(true)
+        element.innerHTML = '<i class="fa-solid fa-right-from-bracket"></i> LOGOUT'
     }
     else {
-        element.innerText = "LOGIN"
+        element.innerHTML = '<i class="fa-solid fa-right-to-bracket"></i> LOGIN'
     }
 }
 function hiddenObj(element){
@@ -11,19 +19,20 @@ function hiddenObj(element){
 }
 var numberpost1 = 24
 function alertChangeLike1(element){
-    numberpost1++
-    alert("This got 1 more like, now is "+numberpost1+" likes")
-    element.innerText = numberpost1 + " likes"    
+    console.log(element.innerText)
+    numberpost1++;
+    alert("This got 1 more like, now is "+numberpost1+" likes");
+    element.innerText = numberpost1 + " likes";
 }
 var numberpost2 = 99
 function alertChangeLike2(element){
-    numberpost2++
+    numberpost2++;
     alert("This got 1 more like, now is "+numberpost2+" likes")
-    element.innerText = numberpost2 + " likes"    
+    element.innerText = numberpost2 + " likes"; 
 }
 var numberpost3 = 18
 function alertChangeLike3(element){
-    numberpost3++
+    numberpost3++;
     alert("This got 1 more like, now is "+numberpost3+" likes")
-    element.innerText = numberpost3 + " likes"    
+    element.innerText = numberpost3 + " likes";    
 }
